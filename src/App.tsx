0a536df25709268a901fdbd100bf4916828abe7d
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-11 22:13:07 -0300

    H1 and Counter componented inserted

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>React Counter</h1>
+      <Counter></Counter>
     </div>
   );
 }
